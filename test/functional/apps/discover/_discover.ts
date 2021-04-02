5402dcf3c7bac48796bad688ca9f310702c5d61d
Tre' Seymour <wayne.seymour@elastic.co>
2021-04-02 13:15:16 -0600

    [QA] Switch tests to use importExport - discover
    Drop the clean call since we are using the empty kibana call here.

--- test/functional/apps/discover/_discover.ts
+++ test/functional/apps/discover/_discover.ts
@@ -30,7 +30,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       log.debug('load kibana index with default index pattern');
 
       await esArchiver.load('empty_kibana');
-      await kibanaServer.savedObjects.clean({ types: ['search', 'index-pattern'] });
       await kibanaServer.importExport.load('discover');
 
       // and load a set of makelogs data
